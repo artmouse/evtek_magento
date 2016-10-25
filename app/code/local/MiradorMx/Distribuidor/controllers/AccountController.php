40e36e0582cfdf7885ca24195beb6aab829fcf41
MarianaVC <mariana.valdivia.carbonell@gmail.com>
2016-10-25 16:32:21 -0500

    override createPostAction para distribuidor

--- app/code/local/MiradorMx/Distribuidor/controllers/AccountController.php
+++ app/code/local/MiradorMx/Distribuidor/controllers/AccountController.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once (Mage::getModuleDir('controllers','Mage_Customer').DS.'AccountController.php');
+require_once Mage::getModuleDir('controllers', 'Mage_Customer') . DS . 'AccountController.php';
 
 /**
  * Class MiradorMx_Distrinbuidor_AccountController
@@ -8,29 +8,157 @@ require_once (Mage::getModuleDir('controllers','Mage_Customer').DS.'AccountContr
  * @package  Distribuidor
  * @author   Mariana Valdivia
  */
-class MiradorMx_Distribuidor_AccountController extends Mage_Customer_AccountController
-{
-    /**
-     * Customer login form page
-     */
-    public function loginAction()
-    {
-        print(Mage::getBaseUrl());
-        if ($this->_getSession()->isLoggedIn()) {
-            $this->_redirect('*/*/');
-            return;
-        }
-        $this->getResponse()->setHeader('Login-Required', 'true');
-        $this->loadLayout();
-        $this->_initLayoutMessages('customer/session');
-        $this->_initLayoutMessages('catalog/session');
-        $this->renderLayout();
-    }
+class MiradorMx_Distribuidor_AccountController extends Mage_Customer_AccountController {
 
+	/**
+	 * Customer login Form
+	 * @return type
+	 */
+	public function loginAction() {
+		print(Mage::getBaseUrl());
+		if ($this->_getSession()->isLoggedIn()) {
+			$this->_redirect('*/*/');
+			return;
+		}
+		$this->getResponse()->setHeader('Login-Required', 'true');
+		$this->loadLayout();
+		$this->_initLayoutMessages('customer/session');
+		$this->_initLayoutMessages('catalog/session');
+		$this->renderLayout();
+	}
 
-    public function registerAction()
-    {
+	/**
+	 * Customer register Form
+	 * @return type
+	 */
+	public function createAction() {
+		if ($this->_getSession()->isLoggedIn()) {
+			$this->_redirect('*/*');
+			return;
+		}
 
-    }
+		$this->loadLayout();
+		$this->_initLayoutMessages('customer/session');
+		$this->renderLayout();
+	}
+	/**
+	 * Distribuidor register Form
+	 * @return type
+	 */
+	public function createdistribuidorAction() {
+		if ($this->_getSession()->isLoggedIn()) {
+			$this->_redirect('*/*');
+			return;
+		}
+
+		$this->loadLayout();
+		$this->_initLayoutMessages('customer/session');
+		$this->renderLayout();
+	}
+
+	public function createPostAction() {
+
+		$errUrl = $this->_getUrl('*/*/create', array('_secure' => true));
+
+		if (!$this->_validateFormKey()) {
+			$this->_redirectError($errUrl);
+			return;
+		}
+
+		/** @var $session Mage_Customer_Model_Session */
+		$session = $this->_getSession();
+		if ($session->isLoggedIn()) {
+			$this->_redirect('*/*/');
+			return;
+		}
+
+		if (!$this->getRequest()->isPost()) {
+			$this->_redirectError($errUrl);
+			return;
+		}
+
+		$customer = $this->_getCustomer();
+
+		try {
+			$errors = $this->_getCustomerErrors($customer);
+
+			if (empty($errors)) {
+				$customer->cleanPasswordsValidationData();
+				$customer->save();
+				$this->_dispatchRegisterSuccess($customer);
+				$this->_successProcessRegistration($customer);
+				return;
+			} else {
+				$this->_addSessionError($errors);
+			}
+		} catch (Mage_Core_Exception $e) {
+			$session->setCustomerFormData($this->getRequest()->getPost());
+			if ($e->getCode() === Mage_Customer_Model_Customer::EXCEPTION_EMAIL_EXISTS) {
+				$url = $this->_getUrl('customer/account/forgotpassword');
+				$message = $this->__('There is already an account with this email address. If you are sure that it is your email address, <a href="%s">click here</a> to get your password and access your account.', $url);
+			} else {
+				$message = $this->_escapeHtml($e->getMessage());
+			}
+			$session->addError($message);
+		} catch (Exception $e) {
+			$session->setCustomerFormData($this->getRequest()->getPost());
+			$session->addException($e, $this->__('Cannot save the customer.'));
+		}
+
+		$this->_redirectError($errUrl);
+	}
+
+	public function createdistribuidorPostAction() {
+
+		$errUrl = $this->_getUrl('*/*/createdistribuidor', array('_secure' => true));
+
+		if (!$this->_validateFormKey()) {
+			$this->_redirectError($errUrl);
+			return;
+		}
+
+		/** @var $session Mage_Customer_Model_Session */
+		$session = $this->_getSession();
+		if ($session->isLoggedIn()) {
+			$this->_redirect('*/*/');
+			return;
+		}
+
+		if (!$this->getRequest()->isPost()) {
+			$this->_redirectError($errUrl);
+			return;
+		}
+
+		$customer = $this->_getCustomer();
+
+		try {
+			$errors = $this->_getCustomerErrors($customer);
+
+			if (empty($errors)) {
+				$customer->cleanPasswordsValidationData();
+				$customer->setData('group_id', 2);
+				$customer->save();
+				$this->_dispatchRegisterSuccess($customer);
+				$this->_successProcessRegistration($customer);
+				return;
+			} else {
+				$this->_addSessionError($errors);
+			}
+		} catch (Mage_Core_Exception $e) {
+			$session->setCustomerFormData($this->getRequest()->getPost());
+			if ($e->getCode() === Mage_Customer_Model_Customer::EXCEPTION_EMAIL_EXISTS) {
+				$url = $this->_getUrl('customer/account/forgotpassword');
+				$message = $this->__('There is already an account with this email address. If you are sure that it is your email address, <a href="%s">click here</a> to get your password and access your account.', $url);
+			} else {
+				$message = $this->_escapeHtml($e->getMessage());
+			}
+			$session->addError($message);
+		} catch (Exception $e) {
+			$session->setCustomerFormData($this->getRequest()->getPost());
+			$session->addException($e, $this->__('Cannot save the customer.'));
+		}
+
+		$this->_redirectError($errUrl);
+	}
 
 }
