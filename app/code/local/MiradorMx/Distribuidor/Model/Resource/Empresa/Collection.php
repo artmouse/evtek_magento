08df014db93105df8a9c83e347664805434b90c9
root <mariana.valdivia.carbonell@gmail.com>
2016-11-08 11:03:39 -0600

    quitamos un comentario feo

--- app/code/local/MiradorMx/Distribuidor/Model/Resource/Empresa/Collection.php
+++ app/code/local/MiradorMx/Distribuidor/Model/Resource/Empresa/Collection.php
@@ -8,14 +8,12 @@
  * Collection de empresa
  */
 class MiradorMx_Distribuidor_Model_Resource_Empresa_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
+
 	/**
 	 * Init collection de solicitud
 	 */
 	public function _construct() {
 		$this->_init('distribuidor/empresa');
 	}
-	/**
-	 * @return int bandera para checar si existe una empresa ya.
-	 */
 
 }
