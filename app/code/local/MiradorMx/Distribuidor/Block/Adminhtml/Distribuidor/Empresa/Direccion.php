f2152bf5e0128eaae83b1543a7acdfaf88739d38
root <mariana.valdivia.carbonell@gmail.com>
2016-11-09 18:15:01 -0600

    agregamos agregar direccion de empresa

---
+++ app/code/local/MiradorMx/Distribuidor/Block/Adminhtml/Distribuidor/Empresa/Direccion.php
@@ -0,0 +1,41 @@
+	<?php
+
+/**
+ * Class MiradorMx_Distribuidor_Block_Adminhtml_Empresa_Direccion
+ * Form container para agregar direcciones
+ * @category MiradadorMx
+ * @package  MiradorMx_Distribuidor
+ * @author   Mariana Valdivia
+ */
+class MiradorMx_Distribuidor_Block_Adminhtml_Distribuidor_Empresa_Direccion extends Mage_Adminhtml_Block_Widget_Form_Container {
+	/**
+	 *
+	 */
+	function _construct() {
+		$this->_blockGroup = 'distribuidor';
+		$this->_controller = 'adminhtml_empresa_direccion';
+
+		parent::_construct();
+	}
+
+	/**
+	 * @return mixed
+	 * Ponemos el text del header
+	 */
+	public function getHeaderText() {
+
+		return Mage::helper('distribuidor')->__('Agregar direcciones a empresa');
+
+	}
+
+	/**
+	 * @return mixed
+	 */
+	protected function _prepareLayout() {
+		if ($this->_blockGroup && $this->_controller && $this->_mode) {
+			$this->setChild('form', $this->getLayout()->createBlock($this->_blockGroup . '/' . $this->_controller . '_' . $this->_mode . '_form'));
+		}
+		return parent::_prepareLayout();
+	}
+
+}
---
+++ app/code/local/MiradorMx/Distribuidor/Block/Adminhtml/Distribuidor/Empresa/Direccion/Form.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Class MiradorMx_Distribuidor_Block_Adminhtml_Registro_Solicitud_Edit_Form
+ * @category MiradorMx
+ * @package  MiradorMx_Distribuidor
+ * @author   Mariana Valdivia
+ */
+class MiradorMx_Distribuidor_Block_Adminhtml_Distribuidor_Empresa_Direccion_Form extends Mage_Adminhtml_Block_Widget_Form {
+
+	/**
+	 * @return mixed
+	 * Form para edit de Solicitud en el admin.
+	 */
+	function _prepareForm() {
+		$form = new Varien_Data_Form(array(
+			'id' => 'edit_form',
+			'action' => $this->getUrl('*/*/savedireccion', array('id' => $this->getRequest()->getParam('id'))),
+			'method' => 'post',
+		));
+		$fieldset = $form->addFieldset('form_form', array('legend' => Mage::helper('distribuidor')->__('Información de dirección')));
+
+		$fieldset->addField('name', 'text', array(
+			'label' => 'Nombre de la empresa',
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'name'));
+		$fieldset->addField('url', 'text', array(
+			'label' => 'Website',
+			'name' => 'url'));
+		$fieldset->addField('rfc', 'text', array(
+			'label' => 'RFC',
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'rfc'));
+		$fieldset->addField('correo', 'text', array(
+			'label' => 'Correo del solicitante',
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'correo'));
+		$fieldset->addField('wholesaler_name', 'text', array(
+			'label' => 'Nombre del solicitante',
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'wholesaler_name'));
+		$fieldset->addField('wholesaler_lastname', 'text', array(
+			'label' => 'Apellido del solicitante',
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'wholesaler_lastname'));
+		$fieldset->addField('mensaje', 'textarea', array(
+			'label' => 'Mensaje del solicitante',
+			'readonly' => true,
+			'name' => 'mensaje'));
+		$fieldset->addField('phone', 'text', array(
+			'label' => 'Teléfono de contacto',
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'phone'));
+		$form->setUseContainer(true);
+		$id = Mage::app()->getRequest()->getParam('id_empresa');
+		$model = Mage::getModel('distribuidor/distribuidor')->load($id);
+		$form->setValues($model->getData());
+		$this->setForm($form);
+
+		return parent::_prepareForm();
+	}
+}
